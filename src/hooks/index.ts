fb6f394735c1db6b0a4355e42daa1cd681c8bd8e
Stanislav Kardash <kardash-stanislav-dev@yandex.ua>
2019-12-17 17:36:12 +0200

    fix default hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,9 @@
-export const enter = ctx => scene => ctx.scene.enter(scene)
-export const reenter = ctx => scene => ctx.scene.reenter(scene)
-export const leave = ctx => () => ctx.scene.leave()
-export const id = ctx => ctx.from.id
-export const username = ctx => ctx.from.username
-export const text = ctx => ctx.message.text
+export const enter = ctx => scene => ctx.scene && ctx.scene.enter(scene)
+export const reenter = ctx => scene => ctx.scene && ctx.scene.reenter(scene)
+export const leave = ctx => () => ctx.scene && ctx.scene.leave()
+export const id = ctx => ctx.from && ctx.from.id
+export const username = ctx => ctx.from && ctx.from.username
+export const text = ctx => ctx.message && ctx.message.text
 export const callbackData = ctx => ctx.callbackQuery && ctx.callbackQuery.data
 export const reply = ctx => (...args) => ctx.reply(...args)
 
